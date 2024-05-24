2f1cd27db4e3659b45ebf9c2b565c6971c629005
Clifford Musyoka <your_email@example.com>
2024-05-24 20:44:40 +0300

    Add buttons Functionality

---
+++ app.js
@@ -0,0 +1,19 @@
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".nextBtn");
+const prevBtn = document.querySelector(".prevBtn");
+
+slides.forEach(function(slide,index){
+    slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+nextBtn.addEventListener('click', function(){
+    counter++;
+});
+nextBtn.addEventListener('click', function(){
+    counter--;
+});
+
+function carousel(){
+    
+}
